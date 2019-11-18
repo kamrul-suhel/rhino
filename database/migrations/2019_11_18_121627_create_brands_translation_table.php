1e618987014d0aa7636b47975748d8ebe1a02a0e
Anton Hutchinson <ah@ontoast.com>
2019-11-18 12:22:18 +0000

    Adding brands translation migration

---
+++ database/migrations/2019_11_18_121627_create_brands_translation_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandsTranslationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands_translation', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 20);
+            $table->timestamps();
+        });
+
+        Schema::table('brands', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands_translation');
+
+        Schema::table('brands', function (Blueprint $table) {
+            $table->string('name');
+        });
+    }
+}
