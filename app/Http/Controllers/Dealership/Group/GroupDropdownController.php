51b1dc058aed3022ada305a152289a484e5f4f8e
kamrul <ka@ontoast.com>
2019-12-10 09:50:29 +0000

    Updating user CRUD (WIP)

---
+++ app/Http/Controllers/Dealership/Group/GroupDropdownController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Dealership\Group;
+
+use App\Group;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GroupDropdownController extends Controller
+{
+    public function index(Request $request){
+        $groups = Group::select(
+            'groups.*',
+            'groups_translation.name',
+            'groups_translation.language_id'
+        )
+            ->leftJoin('groups_translation', 'groups_translation.group_id', '=', 'groups.id')
+            ->where('groups_translation.language_id', $this->languageId);
+
+        $groups = $groups->orderBy('id', 'DESC');
+
+        $groups = $groups->get();
+
+        return response()->json([
+            'groups' => $groups,
+            'total' => $groups->count()
+        ]);
+    }
+}
