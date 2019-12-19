3eefda40f71f047f0a09ab6c74ff06000e8da6dd
kamrul <ka@ontoast.com>
2019-12-19 12:45:38 +0000

    Refactoring event_user_table
    Adding Id column

--- database/migrations/2019_11_15_164054_create_events_users_relationship_table.php
+++ database/migrations/2019_11_15_164054_create_events_users_relationship_table.php
@@ -14,6 +14,7 @@ class CreateEventsUsersRelationshipTable extends Migration
     public function up()
     {
         Schema::create('event_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->unsignedBigInteger('event_id')->index();
             $table->unsignedBigInteger('user_id')->index();
             $table->timestamps();
