4611c6cdadcc9e24ee4409e2aa3c07637d8c8fb7
Joshua Lyness <jl@ontoast.com>
2020-01-09 11:46:09 +0000

    login page changes IE

--- resources/js/pages/login/LoginComponent.vue
+++ resources/js/pages/login/LoginComponent.vue
@@ -10,7 +10,7 @@
                   align-center column nowrap>
             <v-flex>
                 <v-card width="600" class="login-section">
-                    <v-form method="post" v-model="valid" ref="guestLoginGorm">
+                    <v-form method="post" v-model="valid" ref="guestLoginForm">
                         <v-layout column nowrap>
                             <v-flex mt-4 align-self-end xs6 class="language-select stroke-dropdown">
                                 <language-picker
