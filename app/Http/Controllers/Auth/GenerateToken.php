074bc5354a2108222c761069ed04dd515888dfb5
kamrul <ka@ontoast.com>
2020-03-11 10:59:57 +0000

    Implementing generate token

---
+++ app/Http/Controllers/Auth/GenerateToken.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use JWTAuth;
+
+class GenerateToken extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['generateToken']]);
+    }
+
+    public function generateToken(Request $request){
+        $credentials = request(['email', 'password']);
+
+        if (! $token = $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer'
+        ]);
+    }
+}
