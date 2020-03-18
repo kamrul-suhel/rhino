31b9ef2c6f1761a1d4a64cd6096d156411cf90dc
Kamrul <kamrul_sscis@hotmail.com>
2020-03-18 16:09:17 +0000

    Update settings.js

--- resources/js/store/modules/settings.js
+++ resources/js/store/modules/settings.js
@@ -67,7 +67,6 @@ const getters = {
 const actions = {
     fetchSettingFields({commit,dispatch}, payload = null){
         return new Promise((resolve, reject) => {
-            console.log('payload : ', payload)
             const URL = `/api/settings${fn.generateParams(payload)}`
             commit('setIsLoading', true)
             axios.get(URL).then((response) => {
