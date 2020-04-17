87703e211c4dc8778f39dfbf765163d0bceeb7dc
kamrul <ka@ontoast.com>
2020-04-17 16:36:23 +0100

    Update AdminDashboard.vue

--- resources/js/pages/dashboard/AdminDashboard.vue
+++ resources/js/pages/dashboard/AdminDashboard.vue
@@ -3,7 +3,6 @@
         <v-layout row wrap class="mb-5">
             <v-flex xs12 class="pa-0 text-xs-center">
                 <h2>{{`${trans.dashboard}`.toUpperCase()|trans}}</h2>
-                <h2><v-btn @click="onEmailSend()">Send mail Test</v-btn></h2>
             </v-flex>
         </v-layout>
 
@@ -280,12 +279,6 @@
 
             getNameWithStatus(guest){
                 return fn.getGuestNameWithStatus(guest, this.trans)
-            },
-
-            onEmailSend(){
-                axios.get('/admin/sendMail').then((response) => {
-                    console.log('response', response)
-                })
             }
         }
     }
